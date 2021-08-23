22d3d70d6f67b7cf2fefa78063110f66484f2edd
Camila <diniz_camila@outlook.com>
2021-08-22 22:33:11 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -18,3 +18,20 @@ describe('SignUp Controller', () => {
   })
 })
 
+describe('SignUp Controller', () => {
+  // se não tiver a propriedade name no cadastro retorna erro
+  test('Should return 400 if no name is provider', () => {
+    // sut = system under text
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400) //tobe compara o ponteiro do objeto também (objs identicos)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
+})
