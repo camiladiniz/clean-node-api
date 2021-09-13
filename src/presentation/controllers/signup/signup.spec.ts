f6b4925157202b7be737a9094a0fd34c15743e8c
camiladiniz <cadiniz.diniz@gmail.com>
2021-09-12 21:55:54 -0300

    feat: ensure SignUpController reutrns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -232,4 +232,25 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(500) //tobe compara o ponteiro do objeto também (objs identicos)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('Should return 200 if data is provided', () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200) //tobe compara o ponteiro do objeto também (objs identicos)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
+  })
 })
