64434ac9027890f7ba4fdd0b7f711cea5ab4ada4
camiladiniz <cadiniz.diniz@gmail.com>
2021-09-13 21:22:57 -0300

    test: ensure DbAddAccount throws if encrypt throws

--- src/data/usecases/db-account/db-add-account.spec.ts
+++ src/data/usecases/db-account/db-add-account.spec.ts
@@ -36,4 +36,20 @@ describe('DbAddAccountUseCase', () => {
     await sut.add(accountData)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
+
+  // teste para garantir que o erro será encaminhado ao controller
+  // esse tipo de teste é feito para garantir que só o controller tenha try catch, e se houver algum erro o controller receba isso
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut()
+    // mockando uma dependencia
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    // espera que o sut retorne uma exceção se a exceção retorna uma exceção
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
