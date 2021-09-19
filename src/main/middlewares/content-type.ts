20cd20bdda11a44bd37417a56281cceea29a5b72
camiladiniz <cadiniz.diniz@gmail.com>
2021-09-18 22:49:57 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from 'express'
+
+export const contentType = (req: Request, res: Response, next: NextFunction): void => {
+  // temos que chamar enxt dps de todos os middlewares senão requisição fica amarrada
+  res.type('json')
+  next()
+}
