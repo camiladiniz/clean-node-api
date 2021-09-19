8863719a41539ab189d2acdb35e354341257ffb2
camiladiniz <cadiniz.diniz@gmail.com>
2021-09-18 23:42:58 -0300

    feat: ensure SignUp route returns an account on success

---
+++ src/main/config/routes.ts
@@ -0,0 +1,13 @@
+import { Express, Router } from 'express'
+import fg from 'fast-glob'
+
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+  // retorna uma lista com o nome e path completo desses arquivos
+  // fg.sync('**/src/main/routes/**routes.ts').map(async file => {
+  //   const route = (await import(`../../../${file}`)).default
+  //   route(router)
+  // })
+  fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
+}
