e51062e3501854a6bd0b6ed99b685bc6cade3ea3
camiladiniz <cadiniz.diniz@gmail.com>
2021-10-09 17:44:49 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -39,8 +39,8 @@ const makeSut = (): SutTypes => {
 
 const makeFakeServerError = (): HttpResponse => {
   const fakeError = new Error()
-    fakeError.stack = 'any_stack'
-    return serverError(fakeError)
+  fakeError.stack = 'any_stack'
+  return serverError(fakeError)
 }
 
 const makeController = (): Controller => {
@@ -61,7 +61,7 @@ const makeController = (): Controller => {
 
 const makeLogErrorRepository = (): LogErrorRepository => {
   class LogErrorRepositoryStub implements LogErrorRepository {
-    async log (stack: string): Promise<void> {
+    async logError (stack: string): Promise<void> {
       return await new Promise(resolve => resolve())
     }
   }
@@ -90,8 +90,7 @@ describe('LogController Decorator', () => {
 
   test('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
-    
-    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'logError')
     jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(new Promise(resolve => resolve(makeFakeServerError())))
     await sut.handle(makeFakeRequest())
     expect(logSpy).toHaveBeenCalledWith('any_stack')
