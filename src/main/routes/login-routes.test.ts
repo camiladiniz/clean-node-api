006e8fc07777e83f0a2f2687686c38fff0fc57e8
camiladiniz <cadiniz.diniz@gmail.com>
2021-12-05 21:22:40 -0300

    feat: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
+import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    accountCollection = MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -26,6 +30,26 @@ describe('Login Routes', () => {
           email: 'camila@gmail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    // verifica se o body é parseado e o server entenda
+    test('Should return 200 on login', async () => {
+      const password = await (hash('123', 12))
+      const res = await accountCollection.insertOne({
+        name: 'Camila',
+        email: 'camila@gmail.com',
+        password: password
+      })
+      console.log('res', res)
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'camila@gmail.com',
+          password: '123'
         })
         .expect(200)
     })
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
-import { makeSignUpController } from '../factories/signup/signup-factory'
 import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeSignUpController } from '../factories/signup/signup-factory'
+import { makeLoginController } from '../factories/login/login-factory'
 // para não precisar criar as rotas dos controllers manualmente
 
 // export default (router: Router): void => {
@@ -13,4 +14,5 @@ import { adaptRoute } from '../adapters/express-route-adapter'
 export default (router: Router): void => {
   // vai adaptar o controller e vai retornar em um formato que express entende
   router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
