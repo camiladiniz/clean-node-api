8e93122dd9b1e9c65efd355b50f082aafa6e4a97
camiladiniz <cadiniz.diniz@gmail.com>
2021-12-26 23:20:52 -0300

    test: ensure SurveyMongoRepository find survey by id on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -74,4 +74,22 @@ describe('Account Mongo Repository', () => {
       expect(surveys.length).toBe(0)
     })
   })
+
+  describe('loadById()', () => {
+    test('Should load survey by id on success', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      })
+      const sut = makeSut()
+      const survey = await sut.loadById(res.insertedId as any)
+      expect(survey).toBeTruthy()
+    })
+  })
 })
