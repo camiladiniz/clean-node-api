0b669ae256b87bafcbd9e6f5f55b271b3aff3fa6
camiladiniz <cadiniz.diniz@gmail.com>
2021-12-31 17:21:28 -0300

    refactor: update libs

--- jest.config.js
+++ jest.config.js
@@ -1,40 +1,19 @@
-
 module.exports = {
   // definir diretório raiz
-  roots: ['<rootDir>/src'],
-  // collectCoverage: true,
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  roots: ['<rootDir>/tests'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
-  // coverageProvider: 'v8',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
-  // A map from regular expressions to paths to transformers
   transform: { // qualquer texto seguido de .ts
     '.+\\.ts$': 'ts-jest'
   },
   moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1' // sempre que encontrar isso concatena com o diretório raiz
   }
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\",
-  //   "\\.pnp\\.[^\\\\]+$"
-  // ],
-
-  // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
-  // unmockedModulePathPatterns: undefined,
-
-  // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
-
-  // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-  // watchPathIgnorePatterns: [],
-
-  // Whether to use watchman for file crawling
-  // watchman: true,
 }
